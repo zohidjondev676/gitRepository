bf2fa489fea6b50c1012fb3f053ddaf4d42c2eb8
zohidjondev676 <zohidjonzohidjondev@gmail.com>
2023-12-05 01:35:38 -0800

    User qo'shildi
    Signed-off-by: zohidjondev676 <zohidjonzohidjondev@gmail.com>

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    private String name;
+    private String password;
+
+}
